20825dcb710c7a90d69cfceb2d02fe85f1b84f1d
Natsuki <u452936i@ecs.osaka-u.ac.jp>
2025-08-25 22:11:37 +0900

    add command G

--- actuator_feedback/actuator_feedback.ino
+++ actuator_feedback/actuator_feedback.ino
@@ -15,6 +15,8 @@ const int SPEED = 255;
 
 int initialAngle = 0;  // 角度の初期値（水平時の値）
 
+int stopFlag = 0;
+
 void setup() {
   pinMode(motorPinA1, OUTPUT);
   pinMode(motorPinA2, OUTPUT);
@@ -43,6 +45,7 @@ void setup() {
 }
 
 void loop() {
+  stopFlag = 0;
   getAngle();
   if (Serial.available() > 0) {
     char c = Serial.read();
@@ -68,14 +71,19 @@ void loop() {
         Serial.println("Command: S (Both Reverse)");
         break;
 
-      case 'A':           // 左相当 → A縮める, B伸ばす
-        moveToAngle(30);  // 反時計回り30度
-        Serial.println("Command: A (A Reverse, B Forward - 反時計回り30度)");
+      case 'A':           // 左相当 → A伸ばす, B縮める
+        moveToAngle(-30);  // 反時計回り30度
+        Serial.println("Command: A (A Forward, B Reverse - 反時計回り30度)");
         break;
 
-      case 'D':            // 右相当 → A伸ばす, B縮める
-        moveToAngle(-30);  // 時計回り30度
-        Serial.println("Command: D (A Forward, B Reverse - 時計回り30度)");
+      case 'D':            // 右相当 → A縮める, B伸ばす
+        moveToAngle(30);  // 時計回り30度
+        Serial.println("Command: D (A Reverse, B Forward - 時計回り30度)");
+        break;
+      
+      case 'G':  //進行中の感じ
+        Serial.println("Command: G (random move)");
+        driveMode();
         break;
 
       default:  // その他キー → 停止
@@ -90,23 +98,36 @@ void loop() {
 
 // 角度を指定して動かす
 void moveToAngle(int targetRelativeAngle) {
+  Serial.print("move to ");
+  Serial.println(targetRelativeAngle);
   int tolerance = 5;  // 許容誤差
 
   int targetAngle = initialAngle + targetRelativeAngle;  // 目標の絶対角度(可変抵抗の角度) を計算
 
   while (1) {
+    Serial.println("while");
+    if (Serial.available() > 0) {
+    char c = Serial.read();
+    if (c == 'Q'){
+      Serial.println("Command: Q (stop moveToAngle command)");
+      stopFlag = -1;
+      break; //中断できるように 
+    }
+    }
+    /*
     Serial.print(targetRelativeAngle);
     Serial.print("\t");
     Serial.print(targetAngle);
     Serial.print("\t");
     Serial.println(getAngle());
+    */
 
     if (tolerance < getAngle() - targetAngle) {
-      Serial.println("時計回り");
+      Serial.println("反時計回り");
       forwardA();
       reverseB();
     } else if (tolerance < targetAngle - getAngle()) {
-      Serial.println("反時計回り");
+      Serial.println("時計回り");
       reverseA();
       forwardB();
     } else {
@@ -117,17 +138,43 @@ void moveToAngle(int targetRelativeAngle) {
   stopB();
 }
 
+void driveMode(){
+  while(1){
+    if(stopFlag == -1){ //中断コマンドでdriveModeからも抜けられるように
+      Serial.println("Command: Q (stop driveMode)");
+      break;
+    }else{
+      int randomInt = random(-15, 15); //新しい目標角度を設定
+      Serial.print("randomInt: ");
+      Serial.println(randomInt);
+      moveToAngle(randomInt);
+    }
+  }
+  stopA();
+  stopB();
+  
+}
+
 int getAngle() {
   int analogin;                 // アナログ入力値を代入する変数
-  analogin = getAnalogInput();  // アナログ入力ソケット入力値を変数に代入
+  //analogin = getAnalogInput();  // アナログ入力ソケット入力値を変数に代入
+  //アナログ入力の平均を取るように変更
+  int analogin1 = getAnalogInput();
+  delay(5);
+  int analogin2 = getAnalogInput();
+  delay(5);
+  int analogin3 = getAnalogInput();
+  analogin = (analogin1 + analogin2 + analogin3) / 3;
+  //ここまで変更内容
 
   int angle = map(analogin, 0, 4095, 0, 270);       // アナログ入力レンジ => 角度指示レンジに変換
   int percentage = map(analogin, 0, 4095, 0, 100);  // アナログ入力レンジ => アナログ出力レンジに変換
-
+/*
   Serial.print("analogin: ");
   Serial.print(analogin);
   Serial.print(", Angle: ");
-  Serial.println(angle);
+  Serial.print(angle);
+  */
 
   return angle;
 }
